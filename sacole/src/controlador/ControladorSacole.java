f12df8256c5e203b83b5bb73953324cb90a390d4
Administrador <Administrador@10.10.4.18>
2019-10-01 16:49:11 -0300

    oi

--- sacole/src/controlador/ControladorSacole.java
+++ sacole/src/controlador/ControladorSacole.java
@@ -13,6 +13,11 @@ import javax.swing.JOptionPane;
 import modelo.Sacole;
 import tela.manutencao.ManutencaoSacole;
 
+import java.util.List;
+
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -20,22 +25,97 @@ import tela.manutencao.ManutencaoSacole;
  */
 public class ControladorSacole {
 
-   public static void inserir(ManutencaoSacole man){
+    public static void inserir(ManutencaoSacole man) {
         Sacole objeto = new Sacole();
         objeto.setNrserie(Integer.parseInt(man.jtfNrserie.getText()));
         objeto.setPreco(Double.parseDouble(man.jtfPreco.getText()));
         objeto.setDatavalidade(LocalDate.parse(man.jtfDatavalidade.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         objeto.setSabor(man.jtfSabor.getText());
-        
+
         boolean resultado = DaoSacole.inserir(objeto);
         if (resultado) {
             JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+            if (man.listagem != null) {
+                atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+            }
+            man.dispose();//fechar a tela da manutenção
         } else {
             JOptionPane.showMessageDialog(null, "Erro!");
         }
-}
+    }
 
-    public static void alterar(ManutencaoSacole aThis) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static void alterar(ManutencaoSacole man) {
+        Sacole objeto = new Sacole();
+        //definir todos os atributos
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
+        objeto.setNrserie(Integer.parseInt(man.jtfNrserie.getText()));
+        objeto.setPreco(Double.parseDouble(man.jtfPreco.getText()));
+        objeto.setDatavalidade(LocalDate.parse(man.jtfDatavalidade.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        objeto.setSabor(man.jtfSabor.getText());
+
+        boolean resultado = DaoSacole.alterar(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+            if (man.listagem != null) {
+                atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+            }
+            man.dispose();//fechar a tela da manutenção
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+    public static void excluir(ManutencaoSacole man) {
+        Sacole objeto = new Sacole();
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText())); //só precisa definir a chave primeira
+
+        boolean resultado = DaoSacole.excluir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+            if (man.listagem != null) {
+                atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+            }
+            man.dispose();//fechar a tela da manutenção
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+    public static void atualizarTabela(JTable tabela) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //definindo o cabeçalho da tabela
+        modelo.addColumn("Codigo");
+        modelo.addColumn("Nr de Série");
+        modelo.addColumn("Preco");
+        modelo.addColumn("Data de validade");
+        modelo.addColumn("Sabor");
+        List<Sacole> resultados = DaoSacole.consultar();
+        for (Sacole objeto : resultados) {
+            Vector linha = new Vector();
+
+            //definindo o conteúdo da tabela
+            linha.add(objeto.getCodigo());
+            linha.add(objeto.getNrserie());
+           linha.add(objeto.getPreco());
+           linha.add(objeto.getDatavalidade().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            linha.add(objeto.getSabor());
+
+            modelo.addRow(linha); //adicionando a linha na tabela
+        }
+        tabela.setModel(modelo);
+
+    }
+
+    public static void atualizaCampos(ManutencaoSacole man, int pk) {
+        Sacole objeto = DaoSacole.consultar(pk);
+        //Definindo os valores do campo na tela (um para cada atributo/campo)
+        man.jtfCodigo.setText(objeto.getCodigo().toString());
+        man.jtfNrserie.setText(objeto.getNrserie().toString());
+        man.jtfPreco.setText(objeto.getPreco().toString());
+        man.jtfDatavalidade.setText(objeto.getDatavalidade().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        man.jtfSabor.setText(objeto.getSabor());
+
+        man.jtfCodigo.setEnabled(false); //desabilitando o campo código
+        man.btnAdicionar.setEnabled(false); //desabilitando o botão adicionar
     }
 }
--- sacole/src/dao/DaoSacole.java
+++ sacole/src/dao/DaoSacole.java
@@ -12,6 +12,10 @@ import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
 import modelo.Sacole;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Administrador
@@ -63,5 +67,66 @@ public class DaoSacole {
             return false;
         }
     }
-    
+     public static boolean excluir(Sacole objeto) {
+        String sql = "DELETE FROM sacole WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+     
+     public static List<Sacole> consultar() {
+        List<Sacole> resultados = new ArrayList<>();
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, nrserie, preco, datavalidade, sabor FROM sacole";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Sacole objeto = new Sacole();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                objeto.setNrserie(rs.getInt("nrserie"));
+                objeto.setPreco(rs.getDouble("preco"));
+                objeto.setDatavalidade(rs.getDate("datavalidade").toLocalDate());
+                objeto.setSabor(rs.getString("sabor"));
+                
+                resultados.add(objeto);//não mexa nesse, ele adiciona o objeto na lista
+            }
+            return resultados;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+}
+     public static Sacole consultar(int primaryKey) {
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, nrserie, preco, datavalidade, sabor FROM sacole WHERE codigo=?";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, primaryKey);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Sacole objeto = new Sacole();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                 objeto.setNrserie(rs.getInt("nrserie"));
+                 objeto.setPreco(rs.getDouble("preco"));
+                objeto.setDatavalidade(rs.getDate("datavalidade").toLocalDate());
+                objeto.setSabor(rs.getString("sabor"));
+                return objeto;//não mexa nesse, ele adiciona o objeto na lista
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
 }
